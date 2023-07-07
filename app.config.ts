b548297abcd524447ef14cdb0e6dc3108bd55ad5
Marko Banusic <mbanusic@gmail.com>
2023-07-07 14:35:01 +0200

    update socials

--- app.config.ts
+++ app.config.ts
@@ -2,8 +2,8 @@ export default defineAppConfig({
   // Configure Content Wind here
   cover: '/cover.jpg',
   socials: {
-    twitter: 'Atinux',
-    github: 'Atinux/content-wind'
+    twitter: 'mbanusic',
+    github: 'mbanusic/content-wind'
   },
   prose: {
     h1: {
