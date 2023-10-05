17d57fcd6f374d57f652210933b0a9ba3386cbab
Marko Banusic <mbanusic@gmail.com>
2023-10-05 11:54:41 +0200

    icon fix

--- app.config.ts
+++ app.config.ts
@@ -1,13 +1,22 @@
 export default defineAppConfig({
   // Configure Content Wind here
   cover: '/cover.jpg',
+
   socials: {
     twitter: 'mbanusic',
     github: 'mbanusic/content-wind'
   },
+
   prose: {
     h1: {
       icon: 'carbon:sailboat-coastal'
+    }
+  },
+
+  nuxtIcon: {
+    aliases: {
+      'dark-mode': 'fa6-solid:moon',
+      'light-mode': 'fa6-solid:sun'
     }
   }
 })
